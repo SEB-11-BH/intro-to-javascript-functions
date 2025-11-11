77aa3e03a53acaeb64b2a521521c5669a06d2535
omarakamal <omaraakamal@gmail.com>
2025-11-11 11:16:36 +0300

    update

--- class-code/intro-to-functions/app.js
+++ class-code/intro-to-functions/app.js
@@ -96,3 +96,45 @@ function minusTwoNumbers(num1, num2){
     console.log("coding work")
     
 }
+
+
+
+// 2 ways of defining functions
+
+
+// 1. function decleration
+function areaOfSquare(height, width){
+    return height * width
+}
+
+
+console.log(areaOfSquare(5,4))
+
+
+
+
+// 2. function expression
+const areaOfSquareAgain = function(height, width){return height * width}
+
+
+console.log(areaOfSquareAgain(5,4))
+
+
+
+
+
+// Arrow function
+
+// 3 advantages
+// 1. if I only 1 have parameter I dont need the ()
+// 2. If we are only doing 1 line in my execusion block we dont need the {}
+// 3. If we have 1 line of code it will implicitly return that line
+
+const areaOfSquareAgainAgain = (height, width) =>  height * width
+
+
+
+const exponentNumber = number => number* number
+
+
+console.log(exponentNumber(4))
