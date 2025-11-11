3baeebed2c550558b5a78be3402d59cdd95ba3a3
omarakamal <omaraakamal@gmail.com>
2025-11-11 10:38:35 +0300

    update

--- class-code/intro-to-functions/app.js
+++ class-code/intro-to-functions/app.js
@@ -42,3 +42,49 @@ function greetStudent(student){
 
 greetStudent(studentVar)
 greetStudent('Munther')
+
+
+function addTwoNumbers(num1, num2){
+    console.log(num1 + num2)
+}
+
+
+addTwoNumbers(5,100)
+addTwoNumbers(7,2)
+
+
+function planetHasWater(planet){
+    let something = 0
+    console.log(planet)
+    if(planet === 'Earth'|| planet === 'Mars'){
+
+        return true
+
+    }
+    else{
+        return false
+    }
+}
+
+
+planetResult = planetHasWater('Venus')
+
+
+
+
+
+console.log("planet result " + planetResult)
+
+
+function doesPlanetContainBahrain(planet){
+    if(planet === 'Earth'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+if(planetHasWater('Earth') && doesPlanetContainBahrain('Earth')){
+    console.log("YES IT DOES")
+}
