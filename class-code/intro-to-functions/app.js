c324fdeb054e6c78df661e58da0d603399a1ce53
omarakamal <omaraakamal@gmail.com>
2025-11-11 09:45:40 +0300

    update

---
+++ class-code/intro-to-functions/app.js
@@ -0,0 +1,44 @@
+// console.log("Functions Lesson")
+
+
+// console.log("=================")
+// console.log("Good Morning Ali")
+// console.log("=================")
+
+
+
+
+
+// console.log("=================")
+// console.log("Good Morning Ali")
+// console.log("=================")
+
+
+
+// Functions
+
+// 2 parts of working with functions
+
+
+// 1. declaring a function
+
+// function nameOfFunction(){}
+
+function greetAli(){
+    console.log("Good Morning Ali")
+}
+
+
+// 2. calling the function
+
+// greetAli()
+
+
+
+let studentVar = 'Sajeda'
+function greetStudent(student){
+    console.log('Good Morning ' + student)
+}
+
+greetStudent(studentVar)
+greetStudent('Munther')
